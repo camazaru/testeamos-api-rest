b8669fc3b2d116c5d003a0d04bb44c1cfb5b0600
camazaru <74114161+camazaru@users.noreply.github.com>
2022-10-20 18:49:57 -0500

    Delete service directory

--- service/index.service.js
+++
@@ -1,9 +0,0 @@
-import productosService from './productos.service.js'
-import usuarioService from './usuario.service.js'
-import carritoService from './carrito.service.js'
-
-export default {
-    productosService,
-    usuarioService,
-    carritoService
-}
