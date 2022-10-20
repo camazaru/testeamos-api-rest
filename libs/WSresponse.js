eee89ff21764b23c9dffcf6b18d2acb483dfab65
camazaru <74114161+camazaru@users.noreply.github.com>
2022-10-20 18:49:17 -0500

    Delete libs directory

--- libs/WSresponse.js
+++
@@ -1,9 +0,0 @@
-export class WSresponse{
-    constructor(data,message,error,errorCode)
-    {
-        this.data = data,
-        this.message = message || '',
-        this.error = error || false,
-        this.errorCode = errorCode || 0
-    }
-}
